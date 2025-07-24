2ea53fba9f131612db47f3b8fc10f79799291b9b
Aayushi srivastava <aayushisrivastava83@gmail.com>
2025-07-24 15:53:23 +0530

    Time: 52 ms (46.32%) | Memory: 56.3 MB (26.71%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var reverse = function(x) {
+    let firstDigit;
+    let reverseNum=0;
+    let negative=false;
+    if(x<0){
+        x=Math.abs(x);
+        negative=true;
+    }
+    let n=x;
+    while(n>0){
+        firstDigit=n%10;
+        reverseNum=reverseNum*10+firstDigit;
+        n=Math.floor(n/10);
+    }
+    if(negative){
+        reverseNum=-reverseNum;
+    }
+    let safeRange=Math.pow(2,31);
+    if(reverseNum>(safeRange-2) || reverseNum<(-safeRange)){
+        return 0;
+    }
+    return reverseNum;
+};
