ac260d5208feb6580e1f09e907a23cd0db73cadf
Aayushi srivastava <aayushisrivastava83@gmail.com>
2025-08-06 10:44:12 +0530

    Time: 0 ms (100.00%) | Memory: 57.3 MB (28.84%) - LeetSync

---
+++ 231-power-of-two/power-of-two.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfTwo = function(n) {
+    if(n==1){
+        return true
+    }
+    if(n%2!=0){
+        return false
+    }
+    if(n<1){
+        return false
+    }
+    return isPowerOfTwo(n/2)
+    
+};
