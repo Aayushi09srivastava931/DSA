eccd2ce60690e8f698e8b1f1250e389b03a73616
Aayushi srivastava <aayushisrivastava83@gmail.com>
2025-09-15 00:12:02 +0530

    Time: 0 ms (100.00%) | Memory: 56.1 MB (91.30%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.js
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.js
@@ -10,46 +10,37 @@
  * @param {ListNode} list2
  * @return {ListNode}
  */
-var mergeTwoLists = function(l1, l2) {
-    // without using extra space
-    if(!l1){
-        return l2;
+var mergeTwoLists = function(list1, list2) {
+    if(list1==null && list2==null){
+        return null
     }
-    if(!l2){
-        return l1;
+    if(list1==null){
+        return list2
     }
-    let curr=null;
-    if(l1.val>l2.val){
-        curr=l2;
-        l2=l2.next;
+    if(list2==null){
+        return list1
     }
-    else{
-        curr=l1;
-        l1=l1.next;
-    }
-    let ans=curr;
-    while(l1 && l2){
-        if(l1.val>l2.val){
-           curr.next=l2;
-           l2=l2.next;
-           curr=curr.next; 
+    let temp=list1
+    let trav=list2
+    let dummy=new ListNode()
+    let prev=dummy
+    while(temp && trav){
+        if(temp.val<trav.val){
+            prev.next=temp
+            temp=temp.next
+            prev=prev.next
         }
         else{
-            curr.next=l1
-            l1=l1.next;
-            curr=curr.next;
+            prev.next=trav
+            trav=trav.next
+            prev=prev.next
         }
     }
-    while(l1){
-        curr.next=l1;
-        l1=l1.next;
-        curr=curr.next;
+    if(temp==null){
+        prev.next=trav
     }
-    while(l2){
-        curr.next=l2;
-        l2=l2.next;
-        curr=curr.next;
+    if(trav==null){
+        prev.next=temp
     }
-    return ans
-
+    return dummy.next
 };
